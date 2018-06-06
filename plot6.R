47a13e0ca4e3fd5e2637635c58fd05b63d937916
csmahori <csmahori@gmail.com>
2018-06-06 15:26:23 +0530

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,8 +1,13 @@
-sub6<-subset(NEI,NEI$SCC %in% sub4$SCC & fips == "06037")
-par(mfrow=c(1,2))
-with(sub5,plot(year,Emissions))
-title(main="MotorVehicle Emission-Baltimore city",cex.main=0.8)
-with(sub6,plot(year,Emissions))
-title(main="MotorVehicle Emission-Los Angeles city",cex.main=0.8)
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC<-readRDS("Source_Classification_Code.rds")
+sub4<-SCC[grep("[Mm]otor|[Vv]ehicle",SCC$SCC.Level.Three),]
+sub41<-sub4 %>% merge(NEI,by="SCC") %>% 
+filter(fips=="24510") %>% mutate(year=as.factor(year),city="Baltimore") %>%
+group_by(year,city) %>% summarize(total_emissions=sum(Emissions))
+sub42<-sub4 %>% merge(NEI,by="SCC") %>%
+filter(fips=="06037") %>% mutate(year=as.factor(year),city="Los Angeles") %>%
+group_by(year,city) %>% summarize(total_emissions=sum(Emissions))
+sub43<-rbind(sub41,sub42)
+ggplot(sub43,aes(year,total_emissions,group=city,col=city))+geom_line(size=1.25)+scale_y_log10()
 dev.copy(png,file="plot6.png")
 dev.off()
