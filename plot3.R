ebd9dc8c40e4d83b29c8610a9f874507eb71b8c6
csmahori <csmahori@gmail.com>
2018-06-06 15:24:39 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,2 +1,9 @@
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC<-readRDS("Source_Classification_Code.rds")
 library(ggplot2)
-qplot(year,Emissions,data=sub1,facets=.~type)
+sub11 <- sub1 %>% mutate(afyear = as.factor(year)) %>% 
+group_by(afyear, type) %>% summarize(total_emissions = sum(Emissions, na.rm = TRUE)) %>%
+arrange(desc(total_emissions, by_group = TRUE))
+ggplot(sub11,aes(afyear,total_emissions))+geom_bar(stat = "identity",aes(fill=afyear))+facet_grid(.~type)
+dev.copy(png,file="plot3.png")
+dev.off()
