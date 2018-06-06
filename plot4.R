e474e063bcc0adad933854bcc141db58ab335456
csmahori <csmahori@gmail.com>
2018-06-06 15:25:25 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,5 +1,11 @@
-sub2<-SCC[grep("[Cc]oal",SCC$EI.Sector),]
-str(sub2)
-sub3<-subset(NEI,NEI$SCC %in% sub2$SCC)
-str(sub3)
-qplot(year,Emissions,data=sub3)
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC<-readRDS("Source_Classification_Code.rds")
+sub2<-SCC[grep("[Cc]oal",SCC$SCC.Level.Three),]
+sub21<-sub2 %>% merge(NEI,by="SCC") %>% 
+mutate(year=as.factor(year)) %>% 
+group_by(year,SCC.Level.Three) %>% 
+summarize(total_emissions=sum(Emissions,na.rm=TRUE))
+ggplot(sub21,aes(year,total_emissions,group=SCC.Level.Three,col=SCC.Level.Three))+
+geom_line(size=1.25)+scale_y_log10()
+dev.copy(png,file="plot4.png")
+dev.off()
